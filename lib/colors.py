db4401134277f5fb74964f4c16808252f6dca21a
MrSharkSpamBot <83559791+MrSharkSpamBot@users.noreply.github.com>
2021-11-13 15:35:27 -0800

    Add files via upload

---
+++ lib/colors.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+The GlidingSword colors.
+
+@author: Mr. Shark Spam Bot
+"""
+
+normal = '\033[0m'
+glidingsword = '\033[92m'
+error = '\033[91m'
+success = '\33[32m'
+listeners = '\33[34m'
+payloads = '\33[35m'
