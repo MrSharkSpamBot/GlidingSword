6fb676598c87c8139718622b06b91b5530c5f973
MrSharkSpamBot <83559791+MrSharkSpamBot@users.noreply.github.com>
2021-11-13 18:39:50 -0800

    Update colors.py

--- lib/colors.py
+++ lib/colors.py
@@ -6,8 +6,8 @@ The GlidingSword colors.
 """
 
 normal = '\033[0m'
-glidingsword = '\033[92m'
+glidingsword = '\33[32m'
 error = '\033[91m'
-success = '\33[32m'
+success = '\033[92m'
 listeners = '\33[34m'
 payloads = '\33[35m'
